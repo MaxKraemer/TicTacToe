1bd2f472a78eda75678929245cb33cdf2769cf02
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-05-13 21:51:04 +0200

    add winner

--- script.js
+++ script.js
@@ -35,20 +35,53 @@ function draw(){
 
 function checkForWin(){
 
-    if(fields[0]== fields[1] && fields[1] == fields[2]){
+    let winner;
 
-
-    }
-    
-    if(fields[2]== fields[4] && fields[4] == fields[5]){
-
+    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
+        winner = fields[0];
 
     }
     
-    if(fields[6]== fields[7] && fields[7] == fields[8]){
+    if(fields[3] == fields[4] && fields[4] == fields[5] && fields[4]){
+        winner = fields[3];
 
+    }
+    
+    if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
+        winner = fields[6];
 
     }
 
+    if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
+        winner = fields[0];
+
+    }
+    
+    if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
+        winner = fields[1];
+
+    }
+    
+    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
+        winner = fields[2];
+
+    }
+    
+    if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
+        winner = fields[0];
+
+    }
+    
+    if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]){
+        winner = fields[0];
+
+    }
+    
+    if(winner){
+
+        console.log('GEWONNEN', winner);
+    }
+
+    
 
 }
